4e6e6c92980608760ad9288c42540f04075dd55b
louiebloo <leggioluke5@gmail.com>
2024-08-13 20:59:29 -0700

    Work

--- player.js
+++ player.js
@@ -1,7 +1,12 @@
+const { v4: uuidv4 } = require('uuid');
+
 module.exports = class Player {
 
-    constructor(name) {
-        this.name = name
+    constructor(name, socketId, turnOrder) {
+        this.name = name;
+        this.socketId = socketId;
+        this.id = uuidv4();
+        this.turnOrder = turnOrder;
     }
  
     
