c4928ea3ad23badba075924f4da3dac1b465b44b
louiebloo <leggioluke5@gmail.com>
2024-08-31 16:31:26 -0700

    more spectator work

--- src/app.ts
+++ src/app.ts
@@ -64,7 +64,7 @@ io.on('connection', (socket:any) => {
     
 
     socket.join(roomName);
-    socket.emit('roomJoined', { roomName, socketId: socket.id });
+    //socket.emit('roomJoined', { roomName, socketId: socket.id });
     socket.to(roomName).emit('newPeer', { socketId: socket.id, user: newUser, players: roomState.rooms[roomName].playerSockets, spectators: roomState.rooms[roomName].spectatorSockets });
 
     socket.on('signal', (data:any) => {
@@ -97,6 +97,7 @@ io.on('connection', (socket:any) => {
     socket.on('disconnect', () => {
       console.log('A user disconnected:', socket.id);
       //rooms[roomName] = rooms[roomName].filter((id:any) => id !== socket.id);
+      if(!roomState.rooms[roomName]){return;}
       roomState.rooms[roomName].userDisconnected(socket.id);
       socket.to(roomName).emit('peerDisconnected', { socketId: socket.id });
       if (roomState.rooms[roomName].playerSockets.length === 0) {
