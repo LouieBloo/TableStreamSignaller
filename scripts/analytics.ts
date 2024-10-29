3db1c085466123ccef7c036230f4bc9f6c907d23
louiebloo <leggioluke5@gmail.com>
2024-10-28 19:14:36 -0700

    Mongo integration and analytics

---
+++ scripts/analytics.ts
@@ -0,0 +1,15 @@
+import "../src/mongo/mongo";
+import RoomService from '../src/mongo/services/room-service';
+
+const run = async(daysAgo:number)=>{
+    let startTime = new Date();
+    startTime.setDate(startTime.getDate() - daysAgo);
+
+    let endTime = new Date();
+
+    let results = await RoomService.findRoomsInPeriod(startTime, endTime);
+    console.log("Results: ");
+    console.log(JSON.stringify(results));
+}
+
+run(1);
