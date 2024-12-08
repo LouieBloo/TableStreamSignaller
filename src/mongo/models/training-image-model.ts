87e6fb46480bd8c56d1256f5a3583cedcc7d4a1c
louiebloo <leggioluke5@gmail.com>
2024-12-08 15:25:29 -0800

    Saving images

---
+++ src/mongo/models/training-image-model.ts
@@ -0,0 +1,33 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IMongoTrainingImage extends Document {
+  imageName: string;
+  imageLocation: string;
+  imageType:string;
+  status:string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const TrainingImageSchema: Schema = new Schema(
+  {
+    imageName: { type: String, required: true },
+    imageLocation: { type: String, required: true },
+    imageType: {
+      type: String,
+      enum: ['BOARD', 'CARD'],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['PENDING_SLICE', 'SLICED', 'PENDING_CLASSIFICATION', 'CLASSIFIED'],
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+export default mongoose.model<IMongoTrainingImage>('training_image', TrainingImageSchema);
