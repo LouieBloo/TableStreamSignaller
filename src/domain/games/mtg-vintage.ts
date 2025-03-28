33dc765dab593336af76756a3975f55e7532036f
louiebloo <leggioluke5@gmail.com>
2025-03-28 16:26:45 -0700

    Added parent mtg game class, added initiative

--- src/domain/games/mtg-vintage.ts
+++ src/domain/games/mtg-vintage.ts
@@ -2,9 +2,10 @@ import { Player } from "../users/player";
 import { IGameEvent, GameEvent, ICommanderDamage, GameType } from "../interfaces/IGame";
 import { Game } from "./game";
 import { slimCard } from "../interfaces/ICards";
+import { MTGGame } from "./mtg-game";
 
 
-export class MTGVintage extends Game {
+export class MTGVintage extends MTGGame {
     startingLifeTotal = 20;
     gameType:GameType = GameType.MTGVintage;
     constructor(){
