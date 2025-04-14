566a88d5de66e7bf9916575f40067ec4a9d17691
louiebloo <leggioluke5@gmail.com>
2025-04-14 16:14:11 -0700

    More STUN

--- src/infrastructure/xirsys/xirsys-service.ts
+++ src/infrastructure/xirsys/xirsys-service.ts
@@ -1,6 +1,20 @@
 import axios from 'axios';
 
-const backupIceServers = [{ urls: 'stun:stun.l.google.com:19302' }];
+const backupIceServers = [
+  { urls: 'stun:stun.l.google.com:19302' },
+  { urls: 'stun:stun1.l.google.com:19302' },
+  { urls: 'stun:stun2.l.google.com:19302' },
+  { urls: 'stun:stun3.l.google.com:19302' },
+  { urls: 'stun:stun4.l.google.com:19302' },
+  { urls: 'stun:stun.stunprotocol.org:3478' },
+  { urls: 'stun:stun.ekiga.net' },
+  { urls: 'stun:stun.ideasip.com' },
+  { urls: 'stun:stun.voiparound.com' },
+  { urls: 'stun:stun.voipbuster.com' },
+  { urls: 'stun:stun.voipstunt.com' },
+  { urls: 'stun:stun.counterpath.net' },
+  { urls: 'stun:stun.sipgate.net:10000' }
+];
 
 export const getIceServerList = async (): Promise<any[]> => {
   if (!process.env.XIRSYS_API_KEY) { return backupIceServers }
